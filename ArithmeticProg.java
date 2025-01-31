e5c83709cb882e0ef3e10ae7cd735bc1556b6be1
sufihannany <sufihannany@gmail.com>
2025-01-31 13:08:19 +0800

    Add files via upload

---
+++ ArithmeticProg.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arithmeticprog;
+import java.util.Scanner;
+/**
+ *
+ * @author sufi
+ */
+public class ArithmeticProg {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner read;
+        read = new Scanner(System.in);
+        
+        System.out.print("Enter First number: ");
+        int num1 = read.nextInt();
+        
+        System.out.print("Enter Second number: ");
+        int num2 = read.nextInt();
+        
+        System.out.println("\nResults:");
+        System.out.println("Addition: " + (num1 + num2));
+        System.out.println("Substraction: " + (num1 - num2));
+        System.out.println("Multiplication: " + (num1 * num2));
+        System.out.println("Division: " + (num1 / num2));
+        System.out.println("Modulus: " + (num1 % num2));
+        
+        read.close();
+}
+    
+        
+}
