a13788a865df7afd3da5437d9ea1df97c778eb3e
Fatihah-152 <fhusna152@gmail.com>
2025-02-07 21:28:18 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/TaskForToday_7.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication6_chapter.pkg2.task.pkgfor.today;
+
+/**
+ *
+ * @author Fatihah
+ */
+public class TaskForToday_7 {
+    
+    public static void main(String[] args) {
+        
+        //TFT 7 (Chapter 2)
+        
+        System.out.println("INCREMENT & DECREMENT");
+        System.out.println(" ");
+        System.out.println(" ++VARIABLE (PRE INCREMENT)");
+        System.out.println("kna tambah dulu baru turun bawah");
+        System.out.println("int x = 5\nint y = ++x\noutput: x = 6\n        y = 6");
+        
+        System.out.println(" VARIABLE++ (POST INCREMENT)");
+        System.out.println("Turun dulu, tambah kemudian ");
+        System.out.println("int x = 5\nint y = x++\noutput: x = 6\n        y = 5");
+        
+        System.out.println(" --VARIABLE (PRE DECREMENT)");
+        System.out.println("kna tolak dulu baru turun bawah");
+        System.out.println("int x = 5\nint y = --x\noutput: x = 4\n        y = 4");
+        
+        System.out.println(" VARIABLE-- (POST DECREMENT)");
+        System.out.println("Turun dulu, tolak kemudian ");
+        System.out.println("int x = 5\nint y = x--\noutput: x = 4\n        y = 5");
+        
+    }
+}
