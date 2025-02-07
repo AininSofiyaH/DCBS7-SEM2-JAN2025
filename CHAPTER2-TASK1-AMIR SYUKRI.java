5b69f03ed714d58ac38b14e835d46d0c71a42ba7
amir5em <amirdotsyukri@gmail.com>
2025-02-07 08:59:27 +0800

    Add files via upload

---
+++ CHAPTER2-TASK1-AMIR SYUKRI.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject2;
+
+/**
+ *
+ * @author amird
+ */
+import java.util.Scanner;
+
+public class Mavenproject2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
+        // Declaring integer variables for the results
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
+        int remainder = a % b;
+
+        // Printing results with expressions first
+        System.out.println(a + " + " + b + " = " + sum);
+        System.out.println(a + " - " + b + " = " + difference);
+        System.out.println(a + " * " + b + " = " + product);
+        System.out.println(a + " / " + b + " = " + quotient);
+        System.out.println(a + " % " + b + " = " + remainder);
+
+        sc.close();
+    }
+}
+
+     
+
+
+
+
+
+
