57df12efaab683dd51183a34a3cb6d726edce20c
mhdsirajuddin <0305sirajuddin@gmail.com>
2025-02-07 09:11:12 +0800

    Update CHAPTER2-TASK3-SIRAJUDDIN.java

--- CHAPTER2-TASK_3/CHAPTER2-TASK3-SIRAJUDDIN.java
+++ CHAPTER2-TASK_3/CHAPTER2-TASK3-SIRAJUDDIN.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-		public class task3
+		
 		{
 		public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Tahun anda lahir: ");
+        System.out.print("Tahun anda lahir: ");//suruh user masukkan tahunn mereka lahir
         int tahunlahir = scanner.nextInt();
-        System.out.print("Tahun kini: ");
+        System.out.print("Tahun kini: ");//suruh user masukkan tahun terkini
         int tahunkini = scanner.nextInt();
         int umur = tahunkini - tahunlahir;
         System.out.println("\n--- MANTAP ---");
-        System.out.println("Umur anda ialah ! : " + umur + " tahun");
+        System.out.println("Umur anda ialah ! : " + umur + " tahun");//hasil
         System.out.println("-----------------");
 
     }
