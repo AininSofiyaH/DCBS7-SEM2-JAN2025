b41824a12fd5b4e6ff0932bd4352b8c5cac14070
mhdsirajuddin <0305sirajuddin@gmail.com>
2025-02-07 09:09:18 +0800

    Update CHAPTER2-TASK2-SIRAJUDDIN.java

--- CHAPTER2-TASK_2/CHAPTER2-TASK2-SIRAJUDDIN.java
+++ CHAPTER2-TASK_2/CHAPTER2-TASK2-SIRAJUDDIN.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 		{
 		public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print(" Enter nama penuh : ");
+		
+        System.out.print(" Enter nama penuh : ");//ask user to enter fullname
         String name = scanner.nextLine();
-        System.out.print("Enter umur anda : ");
+        System.out.print("Enter umur anda : ");//ask user to enter age
         int umur = scanner.nextInt();
         scanner.nextLine();
-        System.out.print("Hobi apa?: ");
+        System.out.print("Hobi apa?: ");//ask user to enter hobby
         String hobby = scanner.nextLine();
-        System.out.print("Makanan kegemaran?: ");
+        System.out.print("Makanan kegemaran?: ");//ask user to enter fav food
         String favmakanan = scanner.nextLine();
     
         System.out.println("\n--- Informasi Anda ! -----");
-        System.out.println("--- Name anda: " + name);
+        System.out.println("--- Name anda: " + name);//hasil
         System.out.println("--- Umur anda: " + umur);
         System.out.println("--- Hobi anda: " + hobby);
         System.out.println("--- Makanan kegemaran anda: " + favmakanan);
