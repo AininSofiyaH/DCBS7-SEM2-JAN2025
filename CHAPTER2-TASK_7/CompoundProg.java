7c2718dbc2ed9495e6a392d6e4c77f44d938addc
shamierah <shamierahrasheedah@gmail.com>
2025-02-07 09:10:14 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CompoundProg.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package compoundprog;
+import java.util.Scanner;
+/**
+ *
+ * @author sammy
+ */
+public class CompoundProg {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("++VARIABLES = PRE INCREMENT");
+    System.out.println("The variable is increment before it value used.");
+    System.out.println("");
+    System.out.println("VARIABLES++ = POST INCREMENT");
+    System.out.println("The variable is increment after it value used.");
+    System.out.println("");
+    System.out.println("--VARIABLES = PRE DECREMENT");
+    System.out.println("The variable is -crement before it value used.");
+    System.out.println("");
+    System.out.println("VARIABLES-- = POST DECREMENT");
+    System.out.println("The variable is -crement before it value used.");
+    
+    }
+    
+}
