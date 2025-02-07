445ddb94dce366395ffec37d6b081744454a024b
Goat390 <nabil.azlan79@gmail.com>
2025-02-07 09:15:46 +0800

    Add files via upload

--- CHAPTER2-TASK_6/CHAPTER2-TASK6-NABIL AQIL.java
+++ CHAPTER2-TASK_6/CHAPTER2-TASK6-NABIL AQIL.java
@@ -1,4 +1,4 @@
-
+//Importing the scanner
 import java.util.Scanner;
 
 /**
@@ -7,15 +7,21 @@ import java.util.Scanner;
  */
 public class Task6 {
     public static void main(String[] args) {
+        //Making "in" the scanner
         Scanner in = new Scanner(System.in);
         
+        //Input for height
         System.out.print("Enter your height : ");
         int height = in.nextInt();
         
+        //Input for weight
         System.out.print("Enter your weight : ");
         int weight = in.nextInt();
 
+        //The formula for BMI
         float BMI = weight / ((height / 100) ^ 2);
+        
+        //The print
         System.out.println("\nYour BMI : " + BMI);
     }
 }
