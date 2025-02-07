b4d392efdfd1cdd98cbc8da4a7403076d27ec10d
ziottttt <hazziot0312@gmail.com>
2025-02-07 09:17:57 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_2/CHAPTER2-task_2-MUHAMMAD HAZIQ.java
@@ -0,0 +1,30 @@
+package chapter_2;
+import java.util.Scanner;
+
+public class task_2 {
+   public static void main(String[] args) {
+	
+	   // aim my code is to make a different on this task for practice
+	   // practice and try and error the way to use the value stored on the variable
+	   
+	   String name,old,hobby,favfood; // create variable for 4 string value
+	   
+	  
+	   System.out.println("Nyatakan Nama anda wahai anak muda:");
+	   Scanner botok = new Scanner(System.in);
+	   name = botok.nextLine();
+	    
+	   System.out.println("Sedap woo nama,berapa umur kau?");
+	   old = botok.nextLine();
+	   
+	   System.out.println("Lah dah tua dahh hahaha,hobi kau suka main apa ha?");
+	   hobby =botok.nextLine();
+	   
+	   System.out.println("Fuhh tak penatt ke mat? Apa yang kau makan sampai bole main sukan tuu?");
+	   favfood =botok.nextLine();
+	   
+	   System.out.print("Wahh patut badan kau sado,okay la thankyou sebab luangkan masa dengan aku");
+	   System.out.println("\ntschusss  ^__^");
+	   
+   }
+}
