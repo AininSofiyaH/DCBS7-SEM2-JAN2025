afe113909ba46d835e3e57c904fae98b11f9671d
aariffaadlee <adleeisk@gmail.com>
2025-02-07 09:16:04 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CHAPTER2-TASK7-MUHAMMAD ARIFF ADLEE.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author User
+ */
+public class JavaApplication3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Variables = Pre-Increment, ++a");
+        System.out.println("Pre Increment is used when you need the incremented value immediately in the operation.");
+        System.out.println("The variable is incremented before its value is used in the expression.");
+        System.out.println("Ex: int x = 5");
+        int x = 5;
+       
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println("Second output print");
+        System.out.println(x);
+        
+        System.out.println("VARIABLES = Post Increment, a++");
+        System.out.println("This is used when you need the incremented value after the operation");
+        System.out.println("Ex: int c = 5");
+        int c = 5;
+       
+        System.out.println(c);
+        System.out.println(c++);
+        System.out.println("Second output print");
+        System.out.println(c);
+        
+        System.out.println("VARIABLES = Pre Decrement, --a");
+        System.out.println("This is used when you need the decreased value immediately in the current operation.");
+        System.out.println("Ex: int d = 5");
+        int d = 5;
+        
+        System.out.println(d);
+        System.out.println(--d);
+        System.out.println("Second output print");
+        System.out.println(d);
+        
+        System.out.println("VARIABLES = Post Decrement, a--");
+        System.out.println("This is used when you need the current original value first and decrement happens after the operation.");
+        System.out.println("Ex: int u = 5");
+        int u = 5;
+        
+        System.out.println(u);
+        System.out.println(u--);
+        System.out.println("Second output print");
+        System.out.println(u);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
