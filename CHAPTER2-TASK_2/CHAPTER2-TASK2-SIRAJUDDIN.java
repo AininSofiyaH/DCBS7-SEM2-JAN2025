4d4fc8c3a975140866dd27f2292cbb783098eea6
mhdsirajuddin <0305sirajuddin@gmail.com>
2025-02-07 09:04:17 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_2/CHAPTER2-TASK2-SIRAJUDDIN.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+		public class task2
+		{
+		public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(" Enter nama penuh : ");
+        String name = scanner.nextLine();
+        System.out.print("Enter umur anda : ");
+        int umur = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Hobi apa?: ");
+        String hobby = scanner.nextLine();
+        System.out.print("Makanan kegemaran?: ");
+        String favmakanan = scanner.nextLine();
+    
+        System.out.println("\n--- Informasi Anda ! -----");
+        System.out.println("--- Name anda: " + name);
+        System.out.println("--- Umur anda: " + umur);
+        System.out.println("--- Hobi anda: " + hobby);
+        System.out.println("--- Makanan kegemaran anda: " + favmakanan);
+        
+    }
+}
