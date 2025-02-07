0f967361e411d622b6d06a667be54db503be024b
sufihannany <sufihannany@gmail.com>
2025-02-07 09:02:49 +0800

    Add files via upload

--- CHAPTER2-TASK_3/Task3.java
+++ CHAPTER2-TASK_3/Task3.java
@@ -2,34 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package task3;
-
+package task.pkg3;
+import java.util.Scanner;
 /**
  *
- * @author User
+ * @author sufi
  */
-import javax.swing.JOptionPane;
 public class Task3 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String TheYearYouWereBorn;
-        String TheCurrentYear;
-        int num1, num2, birthyear;
+        Scanner scanner = new Scanner(System.in);
         
-        TheYearYouWereBorn = JOptionPane.showInputDialog("Enter the year you were born.");
-        TheCurrentYear = JOptionPane.showInputDialog("Enter the current year.");
+        System.out.print("Tahun Lahir: ");
+        int birthYear = scanner.nextInt();
+        int currentYear = 2025;
+        int age = currentYear - birthYear;
         
-        num1 = Integer.parseInt(TheYearYouWereBorn);
-        num2 = Integer.parseInt(TheCurrentYear);
-        
-        birthyear = num2 - num1;
-        
-        JOptionPane.showMessageDialog(null, "Your age is "+birthyear, "Results",   JOptionPane.PLAIN_MESSAGE);
-        
-        
+        System.out.println("Anda berumur "  +(age) + " tahun.");
+        scanner.close();
     }
     
 }
