39d1f49f593061a875bdf1f3ad807a6bd1b0b30f
sufihannany <sufihannany@gmail.com>
2025-02-07 15:07:03 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CustomerRequirement1.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package customer.requirement.pkg1;
+
+/**
+ *
+ * @author USER
+ */
+public class CustomerRequirement1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.print("Variable - Pre Increment and Post Increment\n\n");
+         System.out.print("Increment dan Decrement operation untuk menambah dan mengurangkan nilai variable. \n\n");
+         System.out.print("Function : Memudahkan pengurusan operation dalam coding. \n\n");
+         System.out.print("Pre-Increment = Variable is incremented first and new value used in the expression.\n");
+    
+         System.out.print("exp : int x = 5          \n");
+         System.out.print("      int y =++x\n");
+         System.out.println("      Output = x=6, y=6\n\n");
+         System.out.print("Pre-decrement = Variable is dicremented first and new value used in the expression.\n");
+          System.out.print("exp : int x = 5          \n");
+         System.out.print("      int y =--x\n");
+         System.out.println("      Output = x=4, y=4\n\n");
+         
+          System.out.print("Post-Iecrement = Current value of variable used in first experssion,then variable is incremented afterward.\n");
+          System.out.print("exp :  int x = 5          \n");
+         System.out.print("       int y =++x\n");
+         System.out.println("       Output = x=6, y=5\n\n");
+         
+         System.out.print("Post-Decrement = Current value of variable used in first experssion,then variable is decremented afterward.\n");
+          System.out.print("exp :  int x = 5          \n");
+         System.out.print("       int y =--x\n");
+         System.out.println("       Output = x=4, y=5\n\n");
+    }
+    
+}
