09dfc844976378f57c44f2a20f000607f2bcc4eb
Goat390 <nabil.azlan79@gmail.com>
2025-02-07 09:13:14 +0800

    Add files via upload

--- CHAPTER2-TASK_2/CHAPTER2-TASK2-NABIL AQIL.java
+++ CHAPTER2-TASK_2/CHAPTER2-TASK2-NABIL AQIL.java
@@ -1,4 +1,4 @@
-
+//The scanner import
 import java.util.Scanner;
 
 /**
@@ -7,23 +7,29 @@ import java.util.Scanner;
  */
 public class Task2 {
     public static void main(String[] args) {
+        //The variables
         String name, favfood, hobby;
         int age;
         Scanner in = new Scanner(System.in);
         
+        //Input for name
         System.out.print("Enter your name : ");
         name = in.nextLine();
         
+        //Input for age
         System.out.print("Enter your age : ");
         age = in.nextInt();
         in.nextLine();
         
+        //Input for fav food
         System.out.print("Enter your favorite food : ");
         favfood = in.nextLine();
         
+        //Input for hobby
         System.out.print("Enter your hobby : ");
         hobby = in.nextLine();
         
+        //The prints
         System.out.println("\nHere's your stats :-");
         System.out.println("Name : " + name);
         System.out.println("Age : " + age);
