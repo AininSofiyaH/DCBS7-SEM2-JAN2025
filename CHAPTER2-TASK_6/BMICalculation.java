38b2c56756d6d7494b8280bc0e0926709c0250b6
shamierah <shamierahrasheedah@gmail.com>
2025-02-07 09:08:54 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_6/BMICalculation.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bmicalculation;
+import java.util.Scanner; 
+/**
+ *
+ * @author sammy
+ */
+public class BMICalculation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int w;
+        double h;
+        
+        Scanner read = new Scanner(System.in);
+        System.out.println("PLEASE ENTER YOUR WEIGHT IN KILOGRAM : ");
+        w = read.nextInt();
+        System.out.println("PLEASE ENTER YOUR HEIGHT IN METER");
+        h = read.nextDouble();
+        
+        read.close();
+        
+       System.out.println("WEIGHT : " +w);
+       System.out.println("HEIGHT : " +h);
+       double h2 = h * h; //calculation
+       double bmi = w/h2; //bmi formula
+       System.out.println("BMI    : " +bmi);
+       
+    }
+    
+}
