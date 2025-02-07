2ae8098b3649c4eb610abba5a1ad6f9f391f6667
Goat390 <nabil.azlan79@gmail.com>
2025-02-07 09:14:09 +0800

    Add files via upload

--- CHAPTER2-TASK_3/CHAPTER2-TASK3-NABIL AQIL.java
+++ CHAPTER2-TASK_3/CHAPTER2-TASK3-NABIL AQIL.java
@@ -1,4 +1,4 @@
-
+//The scanner import
 import java.util.Scanner;
 
 /**
@@ -7,15 +7,19 @@ import java.util.Scanner;
  */
 public class Task3 {
     public static void main(String[] args) {
+        //Making "in" the scanner
         Scanner in = new Scanner(System.in);
         
+        //Input for current year
         System.out.print("Enter the current year : ");
         int cday = in.nextInt();
         
+        //Input for birth year
         System.out.print("Enter your birthyear : ");
         int bday = in.nextInt();
         in.nextLine();
         
+        //Print the age
         System.out.println("\nYour age is : " + (cday - bday));
     }
 }
