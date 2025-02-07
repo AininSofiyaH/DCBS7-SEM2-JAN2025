6a7c7787f04129d068f9d52e4493b13d1f388bad
shamierah <shamierahrasheedah@gmail.com>
2025-02-07 09:04:58 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_4/StudentInformation.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentinformation;
+import java.util.Scanner;
+/**
+ *
+ * @author sammy
+ */
+public class StudentInformation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name, id, sub;
+        int t1, t2;
+        
+        Scanner read = new Scanner(System.in); //to scan stuudent's input
+        System.out.println("PLEASE ENTER YOUR NAME             : ");
+        name = read.nextLine();
+        System.out.println("PLEASE ENTER YOUR STUDENT ID       : ");
+        id = read.nextLine();
+        System.out.println("PLEASE ENTER YOUR SUBJECT          : ");
+        sub = read.nextLine();
+        System.out.println("PLEASE ENTER YOUR MARKS FOR TEST 1 : ");
+        t1 = read.nextInt();
+        System.out.println("PLEASE ENTER YOUR MARKS FOR TEST 2 : ");
+        t2 = read.nextInt();
+        
+        read.close(); //close scanner
+        
+        System.out.println("NAME         : " +name); //display student input
+        System.out.println("STUDENT ID   : " +id);
+        System.out.println("SUBJECT      : " +sub);
+        int avg = (t1 + t2) / 2;
+        System.out.println("AVERAGE MARK : " +avg);
+    }
+    
+}
