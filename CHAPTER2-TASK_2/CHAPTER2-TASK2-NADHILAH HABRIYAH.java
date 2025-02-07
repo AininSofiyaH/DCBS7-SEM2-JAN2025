02b24e51f2a5bc2b0fa789ddefca0f31d7546b30
Habi <ardhilanad12@gmail.com>
2025-02-07 09:51:14 +0800

    Update CHAPTER2-TASK2-NADHILAH HABRIYAH.java

--- CHAPTER2-TASK_2/CHAPTER2-TASK2-NADHILAH HABRIYAH.java
+++ CHAPTER2-TASK_2/CHAPTER2-TASK2-NADHILAH HABRIYAH.java
@@ -12,21 +12,21 @@ import java.util.Scanner;
 
 public class personalinformation{
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner read = new Scanner(System.in);
         
         // Prompting user for Enter name
         System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        String name = read.nextLine();
         
         System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+        int age = read.nextInt();
         scanner.nextLine();  // Consume the newline
         
         System.out.print("Enter your hobby: ");
-        String hobby = scanner.nextLine();
+        String hobby = read.nextLine();
         
         System.out.print("Enter your favorite food: ");
-        String favoriteFood = scanner.nextLine();
+        String favoriteFood = read.nextLine();
         
         // Displaying the user’s input
         System.out.println("\n--- User Information ---");
@@ -35,6 +35,6 @@ public class personalinformation{
         System.out.println("Hobby: " + hobby);
         System.out.println("Favorite Food: " + favoriteFood);
         
-        scanner.close();
+        read.close();
     }
 }
