8ad9d6553628d7988f1b7a4ceb02cd318173c4af
afeefmizra <afeef0808@gmail.com>
2025-02-07 09:06:28 +0800

    CHAPTER2-TASK1-AFEEFMIZRA.java

--- CHAPTER2-TASK_1/CHAPTER2-TASK1-AFEEFMIZRA.java
+++ CHAPTER2-TASK_1/CHAPTER2-TASK1-AFEEFMIZRA.java
@@ -7,14 +7,17 @@ public static void main(String[] args) {
 	
 	int num1, num2;
 	
-
         Scanner scanner = new Scanner(System.in);
+        
+     // saya menyuruh user meletakkan nombor pertama
         System.out.print("Please masukkan number first anda: ");
         num1 = scanner.nextInt();
-
+        
+     // saya menyuruh user meletakkan nombor kedua
         System.out.print("Okay pastu letak la lagi satu nombor: ");
         num2 = scanner.nextInt();
 
+        // ini bahagian kalkulasi
         System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
         System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
         System.out.println(num1 + " * " + num2 + " = " + (num1 * num2));
