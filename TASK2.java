98160860dca7b115c7b551319788a6c1af688271
aina wo <nanacutiepotato@gmail.com>
2025-02-07 08:59:51 +0800

    Add files via upload

---
+++ TASK2.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package task2;
+import java.util.Scanner;
+/**
+ *
+ * @author linah
+ */
+public class TASK2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+         Scanner Baca = new Scanner(System.in);
+        
+        System.out.print("Nyatakan nama: ");
+        String name = Baca.nextLine();
+        
+        System.out.print("Nyatakan umur: ");
+        int age = Baca.nextInt();
+        
+        Baca.nextLine();
+        
+        System.out.print("Nyatakan hobi: ");
+        String hobby = Baca.nextLine();
+        
+        System.out.print("Nyatakan favourite food anda: ");
+        String favoriteFood = Baca.nextLine();
+      
+        System.out.println("\n ---Maklumat Pengguna--- ");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobby: " + hobby);
+        System.out.println("Favorite Food: " + favoriteFood);
+        
+        Baca.close();
+     }
+    
+     }
