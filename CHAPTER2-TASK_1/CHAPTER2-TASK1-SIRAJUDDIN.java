a794b5114b573bb916308afc6905a73638152a71
mhdsirajuddin <0305sirajuddin@gmail.com>
2025-02-07 08:59:05 +0800

    Update CHAPTER2-TASK1-SIRAJUDDIN.java

--- CHAPTER2-TASK_1/CHAPTER2-TASK1-SIRAJUDDIN.java
+++ CHAPTER2-TASK_1/CHAPTER2-TASK1-SIRAJUDDIN.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 public class ArithmeticProg {
     public static void main(String[] args) {
     
-   int x , y ;
+   int x , y ;//set variable
    
-   Scanner salam = new Scanner(System.in);
-   System.out.print("nombor pertama yang anda ingin kira (x): ");
-   x = salam.nextInt();
+   Scanner oreo = new Scanner(System.in);
+   System.out.print("nombor pertama yang anda ingin kira (x): ");//input untuk x
+   x = oreo.nextInt();
    
-   System.out.print("nombor kedua yang anda ingin kira (y) : ");
-   y = salam.nextInt();
+   System.out.print("nombor kedua yang anda ingin kira (y) : ");//input untuk y
+   y = oreo.nextInt();
    
    
-   System.out.println("kirakira :");
-   System.out.println( x +" + "+ y + " = " +(x+y));
+   System.out.println("kirakira :");//jalan kerja
+   System.out.println( x +" + "+ y + " = " +(x+y));//hasil
    System.out.println( x +" - "+ y + " = " +(x-y));
    System.out.println( x +" * "+ y + " = " +(x*y));
    System.out.println( x +" / "+ y + " = " +(x/y));
