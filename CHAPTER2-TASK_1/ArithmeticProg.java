2bb2b620b1b4596fc809c70dd993489d769ebcb7
AdamDanish <d4mnish@gmail.com>
2025-02-07 09:00:41 +0800

    Add files via upload

--- CHAPTER2-TASK_1/ArithmeticProg.java
+++ CHAPTER2-TASK_1/ArithmeticProg.java
@@ -2,37 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.spg0453;
+package com.mycompany.chapter2;
+
+import java.util.Scanner;
 
 /**
  *
- * @author USER
+ * @author Adam
  */
-import java.util.Scanner;
 public class ArithmeticProg {
     public static void main(String[] args) {
         
-    Scanner kereta = new Scanner(System.in);
-    System.out.print("Masukkan Nilai Pertama: ");
-    int num1 = kereta.nextInt();
-    
-    System.out.print("Masukkan Nilai Kedua: ");
-    int num2 = kereta.nextInt();
-    System.out.println("\n Hasilnya: ");
-    
-    int sum = num1 + num2;
-    System.out.println(num1 + " + " + num2 + " = " + sum);
-    
-    int difference = num1 - num2;
-    System.out.println(num1 + " - " + num2 + " = " + difference);
-    
-    int product = num1 * num2;
-    System.out.println(num1 + " * " + num2 + " = " + product);
-    
-    int division = num1 / num2;
-    System.out.println(num1 + " / " + num2 + " = " + division);
-    
-    int modulus = num1 % num2;
-    System.out.println(num1 + " % " + num2 + " = " + modulus);
-    } 
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Please enter the first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Please enter the second number: ");
+        int num2 = scanner.nextInt();
+        System.out.println("\n Result :");
+        
+        int sum = num1 + num2;
+        System.out.println(num1 + " + " +  num2 + " = " + sum);
+        
+        int difference = num1  - num2;
+        System.out.println(num1 + " - " + num2 + " = " + difference);
+        
+        int product = num1 * num2;
+        System.out.println(num1 + " * " + num2 + " = " + product);
+        
+        int division = num1 / num2;
+        System.out.println(num1 + " / " + num2 + " = " + division);
+        
+        int modulus = num1 % num2;
+        System.out.println(num1 + " % " + num2 + " = " + modulus);
+    }
+       
 }
