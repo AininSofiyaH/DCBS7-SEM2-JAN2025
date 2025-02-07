a0e7a71f6655c040b7dc823b2d7c2a85a9c65a52
Ahmad Raziq <aj1q0306@gmail.com>
2025-02-07 09:10:45 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_5/Task5.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author Asus
+ */
+import java.util.Scanner;
+public class Task5 {
+    public static void main(String[] args){
+        
+        Scanner disc = new Scanner(System.in);
+        System.out.print("Please enter the item name: ");
+        String itemName = disc.nextLine();
+        
+         System.out.print("Please enter the price : ");
+        double priceItem = disc.nextDouble();      
+        
+        System.out.print("Please enter the quantity of the item : ");
+        int quantity = disc.nextInt();  
+        
+        System.out.print("Please enter the discount rate : ");
+        double discountRate = disc.nextDouble();  
+        
+        double totalPrice = priceItem * quantity;
+        double discountAmount = (totalPrice * discountRate) / 100;
+        double finalPrice = totalPrice - discountAmount;
+        
+        System.out.println("\n===== Output =====");
+        System.out.println("Item Name      : " + itemName);
+        System.out.println("Price Item     : " + priceItem);
+        System.out.println("Quantity       : " + quantity);
+        System.out.println("Total Price    : " + totalPrice);
+        System.out.println("Discount Rate  : " + discountRate + "%");
+        System.out.println("Final Price    : " + finalPrice);
+    }
+}
