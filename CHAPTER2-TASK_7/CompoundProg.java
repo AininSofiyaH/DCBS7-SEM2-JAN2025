234d4c8d3ff62442179300d2b0fb913f232cd95d
AdamDanish <d4mnish@gmail.com>
2025-02-07 09:33:58 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CompoundProg.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.chapter2;
+
+/**
+ *
+ * @author Adam
+ */
+public class CompoundProg {
+    public static void main(String[] args) {
+       System.out.println("Pre-Increment [++VAR]");
+       System.out.println("Incremented first, and then the new value is used in the expression");
+       System.out.println("before increment = 4");
+       System.out.println("during increment = 5");
+       System.out.println("after increment = 5");
+       System.out.println("         ");
+       
+       System.out.println("Post-Increment [VAR++]");
+       System.out.println("Value is used first, and then its is incremented");
+       System.out.println("before increment = 5");
+       System.out.println("during increment = 5");
+       System.out.println("after increment = 6");
+       System.out.println("          ");
+       
+       
+       System.out.println("Pre-Decrement [--VAR]");
+       System.out.println("Decremented first, and then the updated value is used in the expression");
+       System.out.println("before decrement = 5");
+       System.out.println("during decrement = 4");
+       System.out.println("after decrement = 4");
+       System.out.println("          ");
+       
+       
+       System.out.println("Post-Decrement [VAR--]");
+       System.out.println("Current value is used in the expression first, and then it is decremented");
+       System.out.println("before decrement = 5");
+       System.out.println("during decrement = 5");
+       System.out.println("after decrement = 4");
+           
+
+   }
+}
