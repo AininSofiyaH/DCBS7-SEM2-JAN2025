39fe5be273cbe1e791dfdff351eb1c4df6b2ef4b
Goat390 <nabil.azlan79@gmail.com>
2025-02-07 09:00:58 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CHAPTER2-TASK7-NABIL AQIL.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author Nabil Aqil
+ */
+public class CompoundProg {
+    public static void main(String[] args) {
+        System.out.println("++VARIABLES - PRE-INCREMENT");
+        System.out.println("It increments, then use the value");
+        System.out.println("Ex. if a = 0, ++a will print out 1");
+        System.out.println("Original Number : 0");
+        System.out.println("Pre-Increment : 1");
+        System.out.println("New Number : 1");
+        System.out.println("");
+        System.out.println("VARIABLES++ - POST-INCREMENT");
+        System.out.println("It use the value, then increments");
+        System.out.println("Ex. if a = 0, a++ will print out 0 but the value has been set to 1");
+        System.out.println("Original Number : 0");
+        System.out.println("Pre-Increment : 0");
+        System.out.println("New Number : 1");
+        System.out.println("");
+        System.out.println("--VARIABLES - PRE-DECREMENT");
+        System.out.println("It decrements, then use the value");
+        System.out.println("Ex. if a = 0, ++a will print out -1");
+        System.out.println("Original Number : 0");
+        System.out.println("Pre-Decrement : -1");
+        System.out.println("New Number : -1");
+        System.out.println("");
+        System.out.println("VARIABLES-- - POST-DECREMENT");
+        System.out.println("It use the value, then decrements");
+        System.out.println("Ex. if a = 0, a++ will print out 0 but the value has been set to -1");
+        System.out.println("Original Number : 0");
+        System.out.println("Pre-Increment : 0");
+        System.out.println("New Number : -1");
+        System.out.println("");
+    }
+}
