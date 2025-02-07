ce56cc0132d952c0207fa48efbf6c1c912b8f15b
aina wo <nanacutiepotato@gmail.com>
2025-02-08 01:50:22 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/task7.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package customer.requirement1;
+import java.util.Scanner;
+/**
+ *
+ * @author aina
+ */
+public class task7 {
+     public static void main(String[] args){
+         // TODO code application logic here
+        
+        
+        //CHAPTER2
+        
+        System.out.println("++VARIABLES = PRE INCREMENT");
+        System.out.println("This increment is used before kita start expression, etc x=5, y= ++x  answers:x=6, y=6");
+        System.out.println("so x tambah 1 then value x turun ke y");
+        System.out.println("VARIABLES++ = POST INCREMENT"); 
+        System.out.println("This incrrement is used after kita dah buat expression, etc x= 5, y= x++ answers:x=6, y=5");
+        System.out.println("so value of x bawak turun to y first then baru value of x tambah 1");
+        
+        System.out.println("");
+        
+        System.out.println("--VARIABLES = PRE DECREMENT");
+        System.out.println("This decrement is used before kita start expression, etc x=5, y= --x  answers:x=4, y=4");
+        System.out.println("so x tolak 1 then value x turun ke y");
+        System.out.println("VARIABLES-- = POST DECREMENT");
+        System.out.println("This decrement is used after kita dah buat expression, etc x= 5, y= x-- answers:x=4, y=5");
+        System.out.println("so value of x bawak turun to y first then baru value of x tolak 1");
+
+    }
+    
+}
