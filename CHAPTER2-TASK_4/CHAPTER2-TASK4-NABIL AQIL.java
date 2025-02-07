6b7bd78dbc4a46dee2f85aae6303bff3c2670e63
Goat390 <nabil.azlan79@gmail.com>
2025-02-07 09:14:43 +0800

    Add files via upload

--- CHAPTER2-TASK_4/CHAPTER2-TASK4-NABIL AQIL.java
+++ CHAPTER2-TASK_4/CHAPTER2-TASK4-NABIL AQIL.java
@@ -1,4 +1,4 @@
-
+//The scanner import
 import java.util.Scanner;
 
 /**
@@ -7,24 +7,30 @@ import java.util.Scanner;
  */
 public class Task4 {
     public static void main(String[] args) {
+        //Making "in" the scanner
         Scanner in = new Scanner(System.in);
         
+        //Input for name
         System.out.print("Enter your name : ");
         String name = in.nextLine();
         
+        //Input for ID
         System.out.print("Enter your ID : ");
         String id = in.nextLine();
         
+        //Input for subject
         System.out.print("Enter your subject : ");
         String subject = in.nextLine();
         
+        //Input for Test 1 Marks
         System.out.print("Enter your marks for Test 1 : ");
         int test1 = in.nextInt();
         
+        //Input for Test 2 Marks
         System.out.print("Enter your marks for Test 2 : ");
         int test2 = in.nextInt();
         
-        
+        //The prints
         System.out.println("\nHere is your results :-");
         System.out.println("Name : " + name);
         System.out.println("ID : " + id);
