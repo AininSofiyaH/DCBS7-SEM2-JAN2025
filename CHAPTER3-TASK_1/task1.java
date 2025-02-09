b1a76be67a9e532fc160ffe5e0356267d13869b4
AdamNazman <adamnerd03@gmail.com>
2025-02-09 13:34:48 +0800

    Add files via upload

---
+++ CHAPTER3-TASK_1/task1.java
@@ -0,0 +1,69 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class task1 { public static void main(String[]args) {
+	String Name , Id , Subject; //naming some variable for string
+	int test1 , test2 , avg; // some for integer
+	
+	System.out.println("Please enter your name below ");
+	System.out.print("Name = ");
+	Scanner hola = new Scanner(System.in);
+	Name = hola.nextLine(); //store the name value stated by the user
+	
+	System.out.println("\nHello there " + Name );
+	System.out.println("Let me help you calculate your average mark from two test");
+	System.out.println("But first enter you student id ");
+	System.out.print("Id = ");
+	Id = hola.nextLine(); // getting their id number and stored in variable Id
+	
+	System.out.println("\nNow your beloved subject ");
+	System.out.print("Subject = ");
+	Subject = hola.nextLine(); // next for the subject 
+	
+	System.out.println("\nNow insert the mark percentage of your first test");
+	System.out.print("First test  = ");
+	test1 = hola.nextInt(); // now for the first test
+	
+	System.out.println("\nNow for your second test");
+	System.out.print("Second test = ");
+	test2 = hola.nextInt(); // this for second test
+	
+	avg = (test1 + test2) / 2; // simple calculation for average
+	
+	System.out.println("\nResult :"); // Output of the user info letsgooo
+	
+	System.out.println("Name of the good looking student");
+	System.out.println("=The Eye-Catching " + Name + "=");
+	
+	System.out.println("\nId of the marvelous student");
+	System.out.println("=" + Id + "=");
+	
+	System.out.println("\nBeloved subject of the student");
+	System.out.println("=Very fun and exciting " + Subject + "=");
+	
+	System.out.println("\nAverage mark of the subject");
+	System.out.println("="+avg+"=");
+	
+	
+	if(avg >= 80) { System.out.println("Grade = A");}
+	else if(avg >= 70) { System.out.println("Grade = B"); }
+	else if(avg >= 60) { System.out.println("Grade = C");}
+	else if(avg >= 50) { System.out.println("Grade = D");}
+	else if(avg >= 40) { System.out.println("Grade = E");}
+	else { System.out.print("Grade = F");
+	//Some words from me
+	
+	System.out.println("\nThe numbers may go up or down, but the one who tried remains strong");
+	System.out.println("because every result is just a step, and every step teaches something important");
+	
+}
+
+	
+}}
+	
+	
+	
+
+
+
