387d4830a58289bdb0c180241e272b64a1ea8607
AdamNazman <adamnerd03@gmail.com>
2025-02-09 13:22:13 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_6/task6.java
@@ -0,0 +1,24 @@
+package Chapter2;
+
+import java.util.Scanner;
+
+public class task6 {public static void main(String[] args) {
+	
+	float w , h ; // create variable for weight and height	
+	
+	System.out.println("======== BMI Calculator ========\n"); // some designs
+	System.out.print("Enter your weight (KG) = ");
+	Scanner hola = new Scanner(System.in);
+	w = hola.nextFloat(); // getting weight
+	
+	System.out.print("Enter your height (M) = ");
+	h = hola.nextFloat(); // getting height
+	
+	System.out.println("Your BMI is = " + String.format("%.2f" ,w/(h*h))); //calculate using formula from slide
+	System.out.println("\n================================");
+	
+	
+}
+
+
+}
