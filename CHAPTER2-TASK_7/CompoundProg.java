7908ac83335c476a13037809f3b29edc027295ce
AdamNazman <adamnerd03@gmail.com>
2025-02-09 13:19:10 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CompoundProg.java
@@ -0,0 +1,32 @@
+package Chapter2;
+
+public class CompoundProg {public static void main(String[] args) {
+	
+	int x = 5; // setting variable name
+	
+	System.out.println("Explaining Increment and Decrement\n");
+	
+	System.out.println("For Pre-Increment : (++x)");
+	System.out.print("Before Pre-Increment (x) = " + x +"\n"); // showing result before
+	System.out.print("Pre-Increment (++x) = " + (++x) +"\n");
+	System.out.print("After Pre-Increment (x) = " + x +"\n\n"); // result after
+	
+	System.out.println("For Post-Increment : (x++)");
+	System.out.print("Before Post-Increment (x) = " + x +"\n"); // same goes for Post-Increment
+	System.out.print("Pre-Increment (x++) = " + (x++) +"\n");
+	System.out.print("After Post-Increment (x) = " + x +"\n\n");
+	
+	System.out.println("For Pre-Decrement : (--x)"); // next showing for pre decrement
+	System.out.print("Before Pre-Decrement (x) = " + x +"\n");
+	System.out.print("Pre-Decrement (--x) = " + (--x) +"\n"); // during Pre-Decrement
+	System.out.print("After Pre-Decrement (x) = " + x +"\n\n");
+	
+	System.out.println("For Post-Decrement : (x--)");
+	System.out.print("Before Post-Decrement (x) = " + x +"\n");
+	System.out.print("Post-Decrement (x--) = " + (x--) +"\n");
+	System.out.print("After Post-Decrement (x) = " + x +"\n\n");
+	
+	
+}
+
+}
