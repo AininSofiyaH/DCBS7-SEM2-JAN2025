d84ceb38a3846367976635467fa06cb4ffc2e66a
reen <azreenzulaikha02@gmail.com>
2025-02-11 23:06:10 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_7/CHAPTER2_TASK7_NURAZREEN.java
@@ -0,0 +1,20 @@
+public class CHAPTER2_TASK7_NURAZREEN {
+
+    public static void main(String[] args) {
+        System.out.println("INCREMENT & DECREMENT");
+
+        System.out.println(" ");
+        System.out.println(" ++ VARIABLE (PRE INCREMENT)");
+        System.out.println("int e = 6 \n int z  = ++e \n output: e = 7 \n       y =7");
+        
+        System.out.println(" VARIABLE ++ (POST INCREMENT)");
+        System.out.println("int e = 6 \n int z = e++ \n output: e = 7 \n        z = 6");
+        
+        System.out.println(" -- VARIABLE (PRE DECREMENT)"); 
+        System.out.println("int e = 6 \n int z = --e \n output: e = 5 \n        z = 5");
+        
+        System.out.println(" VARIABLE -- (POST DECREMENT)");
+        System.out.println("int e = 6 \n int z = e-- \n output: e= 5\n        z = 6");
+        
+    }
+}
