64b16ff0f36af58fa5b44f40aa396274a680535b
Fatihah-152 <fhusna152@gmail.com>
2025-02-13 22:16:31 +0800

    Add files via upload

---
+++ CHAPTER3-TASK_4/TaskForToday_4.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter3;
+import java.util.Scanner;
+/**
+ *
+ * @author Fatihah
+ */
+public class TaskForToday_4 {
+    public static void main(String[] args) {
+        
+    //TFT 4 (Chapter 3)
+    String item_name;
+    int quant, code;
+    //declare variable
+    
+    //Menu
+    System.out.println("welcome to:-    ");
+    System.out.println("         <   HOTPOT SOUP   >         ");
+    System.out.println("1. Vegetable Soup  (01)      RM 11.00");
+    System.out.println("2. Beef Soup       (02)      RM 17.00");  
+    System.out.println("3. Chicken Soup    (03)      RM 15.00");
+    System.out.println("4. Crab Soup       (04)      RM 20.00");
+    System.out.println("");
+    
+    Scanner read = new Scanner(System.in);
+    //supaya user boleh masukkan input
+    
+    System.out.print("Item Name: ");
+    item_name = read.nextLine();
+    //user masukkan nama makanan yg diorang nak
+    
+    System.out.print("Quantity: ");
+    quant = read.nextInt();
+    //user masukkan brapa banyak makanan yg diorang nak
+    
+    System.out.print("Code: ");
+    code = read.nextInt(); 
+    
+    //code untuk setiap makanan
+    //terus kira total price dia
+     switch (code){
+            
+            case 01: System.out.println("\nYour order: \nVegetable Soup  -->  RM "  +(11 * quant));
+            break;
+            case 02: System.out.println("\nYour order: \nBeef Soup  -->   RM " +(17 * quant));
+            break;
+            case 03: System.out.println("\nYour order: \nChicken Soup  -->   RM " +(15 * quant));
+            break;
+            case 04: System.out.println("\nYour order: \nCrab Soup  -->   RM " +(20 * quant));
+            break;
+            default: System.out.println("NO ORDER");
+            break;   
+  
+    }
+    
+     // some interactive word
+    System.out.println("");
+    System.out.println("!! Thank You for Coming !!    ");
+    System.out.println("~~ Please Come Again ~~      "); 
+    System.out.println("");
+}
+}
