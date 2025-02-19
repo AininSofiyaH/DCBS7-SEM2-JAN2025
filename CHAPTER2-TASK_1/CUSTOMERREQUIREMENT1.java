d447b52101242cd01f930e8e2c839f402fd2a6bc
coco <adrianahizadin@gmail.com>
2025-02-19 21:34:55 +0800

    Add files via upload

---
+++ CHAPTER2-TASK_1/CUSTOMERREQUIREMENT1.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package customer.requirement.pkg1;
+
+/**
+ *
+ * @author adria
+ */
+public class CUSTOMERREQUIREMENT1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("===== THIS IS ARTITHMETICS OPERATORS =====");
+        System.out.println("                                         ");
+        System.out.println("           1. 5 + 12 = 17                ");
+        System.out.println("           2. 3 - 1 = 2                  ");
+        System.out.println("           3. 5 / 5 = 1                  ");
+        System.out.println("           4. 12 X 12 = 144              "); 
+        System.out.println("==========================================");
+       
+       
+       
+         System.out.println(" ++ VARIABLES - PRE INCREMENT ");
+         System.out.println("this increment use when nombor tu ditambah dahulu sebelum pergi ke y");
+         System.out.println("example to apply this increment: x=5  , y=++x  , x tambah jadi 6 dulu baru ke y : x=6, y=6 ");
+          System.out.println("                                                                                             ");
+         
+         System.out.println("VARIABLES ++ - POST INCREMENT");
+         System.out.println("this increment use when nombor tu pergi ke y dulu, then dia baru tambah dekat x");
+         System.out.println(" example to apply this increment: x=5  , y=x++  , x pergi dekat y dulu, then dia baru dia tambah : x=6, y=5 ");
+          System.out.println("                                                                                                             ");
+        
+         System.out.println("-- VARIABLES - PRE DECREMENT");
+         System.out.println("this decrement use when nombor tu ditolak dulu sebelum dia pergi ke y  ");
+         System.out.println("example to apply this decrement: x=5 , y=--x , x tolak dulu jadi 4 dulu baru pergi ke y : x=4, y=4 ");
+         System.out.println("                                                                                                  ");
+          
+         System.out.println("VARIABLES-- - POST DECREMENT");
+         System.out.println("this decrement use when nombor tu pergi dulu ke y, then dia baru tolak balik dekat x");
+         System.out.println("example to apply this decrement: x=5  , y=x-- , x pergi dekat y dulu baru dia tolak : x=5,  y=4 ");
+    }    
+    
+    
+}
