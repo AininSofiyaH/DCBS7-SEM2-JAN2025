4e5bff7223d806235a567cb6ea117b9098f9ab58
mhdsirajuddin <0305sirajuddin@gmail.com>
2025-02-20 18:05:08 +0800

    Update CHAPTER2-TASK6-SIRAJUDDIN.java

--- CHAPTER2-TASK_6/CHAPTER2-TASK6-SIRAJUDDIN.java
+++ CHAPTER2-TASK_6/CHAPTER2-TASK6-SIRAJUDDIN.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 		public class task6
 		{
 		public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner abu = new Scanner(System.in);
 
         System.out.print("Berat anda  (kg): ");
-        double weight = scanner.nextDouble();
+        double weight = abu.nextDouble();
         System.out.print("Tinggi anda (m): ");
-        double height = scanner.nextDouble();
+        double height = abu.nextDouble();
         double bmi = weight / (height * height);
         System.out.println("\n--- BMI Result ---");
         System.out.printf("BMI anda: %.2f%n", bmi);
