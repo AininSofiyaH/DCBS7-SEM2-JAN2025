4487aab724381cbd66b8d899fdb7bdde71bbd70d
mhdsirajuddin <0305sirajuddin@gmail.com>
2025-02-20 18:00:54 +0800

    Update CHAPTER2-TASK4-SIRAJUDDIN.java

--- CHAPTER2-TASK_4/CHAPTER2-TASK4-SIRAJUDDIN.java
+++ CHAPTER2-TASK_4/CHAPTER2-TASK4-SIRAJUDDIN.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 		public class task4 
 		{
    		public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner abu = new Scanner(System.in);
 
         System.out.print("Enter Student's Name: ");
-        String name = scanner.nextLine();
+        String name = abu.nextLine();
 
         System.out.print("Enter Student's ID: ");
-        String id = scanner.nextLine();
+        String id = abu.nextLine();
 
         System.out.print("Enter Subject Name: ");
-        String subject = scanner.nextLine();
+        String subject = abu.nextLine();
 
         System.out.print("Test 1 Marks (out of 100): ");
-        double test1 = scanner.nextDouble();
+        double test1 = abu.nextDouble();
 
         System.out.print("Test 2 Marks (out of 100): ");
-        double test2 = scanner.nextDouble();
+        double test2 = abu.nextDouble();
 
         double average = (test1 + test2) / 2;
         System.out.println("\n--- Student's Details ---");
