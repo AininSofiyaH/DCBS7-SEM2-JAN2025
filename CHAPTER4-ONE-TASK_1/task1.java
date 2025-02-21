a66583458661d5f1f60bd7e6c2249b5c7c974322
AdamNazman <adamnerd03@gmail.com>
2025-02-22 07:01:25 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_1/task1.java
@@ -0,0 +1,17 @@
+package Chapter4;
+
+public class task1 {public static void main(String[]args) {
+	System.out.println("List of number 1 to 10 using for loop:");
+	for(int i = 1;i <= 10; i++) { // using for loop to print 1 to 10
+		System.out.println(i);
+	}
+	System.out.println("\nList of number 1 to 10 using while loop:");
+	int i = 1;
+	while (i <= 10) { //using while loop to print 1 to 10
+		System.out.println(i++);
+		
+	}
+}
+
+
+}
