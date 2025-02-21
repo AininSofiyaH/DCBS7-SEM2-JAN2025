2e0a5bd4eaada256f55d98aae6557d92b6504b35
AdamNazman <adamnerd03@gmail.com>
2025-02-22 07:14:22 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_5/task5.java
@@ -0,0 +1,11 @@
+package Chapter4;
+
+public class task5 {public static void main(String[]args) {
+	for(int i = 9;i >=1 ;i--) {//starting with 9 minus 1 if more than or equal to 1
+		System.out.print(times(i)+" ");//print i but undergo method of times
+	}
+}
+public static int times(int y) {//making method where we multiply by 11
+	return y * 11 ;
+}
+}
