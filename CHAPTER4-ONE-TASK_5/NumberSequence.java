5da905b635e17ad63d67e3c4e5855cf05a894597
Habi <ardhilanad12@gmail.com>
2025-02-22 11:49:27 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_5/NumberSequence.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numbersequence;
+
+/**
+ *
+ * @author Habriyah
+ */
+public class NumberSequence {
+    public static void main(String[] args) {
+        // Menggunakan for loop untuk menghasilkan urutan nombor
+        for (int i = 99; i >= 11; i -= 11) { //Menggunakan "for" loop dengan i bermula dari 99 dan dikurangkan secara langsung sebanyak 11 setiap kali.
+            System.out.print(i + " "); // Cetak nombor dengan ruang di antara
+        }
+    }
+}
