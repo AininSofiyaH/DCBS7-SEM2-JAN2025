1c46f7477b6bfba8454beb939c3d9405910a9f2a
reen <azreenzulaikha02@gmail.com>
2025-02-23 20:47:02 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_1/CHAPTER4_TASK1_NURAZREEN.java
@@ -0,0 +1,28 @@
+public class CHAPTER4_TASK1_NURAZREEN {
+    public static void main(String[] args) {
+        //  for loop
+        System.out.println("For Loop:");
+        for (int L = 1; L <= 10; L++) {
+            System.out.print(L + " ");
+        }
+        System.out.println("\n");
+
+        // while loop
+        System.out.println("While Loop:");
+        int Z = 1;
+        while (Z <= 10) {
+            System.out.print(Z + " ");
+            Z++;
+        }
+        System.out.println("\n");
+
+        // do-while loop
+        System.out.println("Do-While Loop:");
+        int E = 1;
+        do {
+            System.out.print(E + " ");
+            E++;
+        } while (E <= 10);
+        System.out.println();
+    }
+}
