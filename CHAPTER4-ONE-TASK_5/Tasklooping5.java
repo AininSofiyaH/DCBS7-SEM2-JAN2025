982c2984887d9f8b039a9705dfac48e3248e4291
aina wo <nanacutiepotato@gmail.com>
2025-02-28 00:51:58 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_5/Tasklooping5.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tasklooping5;
+
+/**
+ *
+ * @author aina
+ */
+public class Tasklooping5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("Nombor sequence:"); //print nomor sequence nya
+        
+        for (int i = 99; i >= 11; i -=11){ //syntax yg harus di fulfill sebelum di print
+        System.out.print(i + " "); //jika pass syntax lalu keluar output nya
+    }
+}
+}
+    
