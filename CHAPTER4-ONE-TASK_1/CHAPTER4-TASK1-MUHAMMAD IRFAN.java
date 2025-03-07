96a9a7219314f82406da2ec500cb5815c5f28c12
irfanhusaini <irfan0102660714@gmail.com>
2025-03-07 08:18:44 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_1/CHAPTER4-TASK1-MUHAMMAD IRFAN.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tasklooping1;
+
+/**
+ *
+ * @author irfan
+ */
+public class TaskLooping1 {
+
+    public static void main(String[] args) {
+        
+    System.out.println("Print number (1-10)");
+    
+    System.out.println("For Loop:");
+    for (int i = 1; i <= 10; i++){
+    System.out.print(i + " ");
+    }
+    System.out.println("\n");
+    
+    System.out.println("While Loop:" );
+    int a = 1;
+    while (a <= 10){
+    System.out.print(a+" ");a++;
+}
+}
+    }
