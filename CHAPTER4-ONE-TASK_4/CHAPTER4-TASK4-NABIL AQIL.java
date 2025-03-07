3bb8b990818e993f72acab3fa518f225b5393509
Goat390 <nabil.azlan79@gmail.com>
2025-03-07 10:08:19 +0800

    Add files via upload

--- CHAPTER4-ONE-TASK_4/CHAPTER4-TASK4-NABIL AQIL.java
+++ CHAPTER4-ONE-TASK_4/CHAPTER4-TASK4-NABIL AQIL.java
@@ -9,12 +9,14 @@ public class Task {
         int sum = 0;
         System.out.println("Power of 1 - 10:");
         
+        //"For loop" for power of n
         for (int n = 1; n <= 10; n++) {
             int power = (int) Math.pow(n, 2);
             System.out.println(n + " * " + n + " = " + power);
             sum += power;
         }
 
+        //Prints the sum of the powers
         System.out.println("\nSum of n ^ 2 from 1 to 10: " + sum);
     }
 }
