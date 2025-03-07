3b62f42a698b98fbbcb7bc59cd8da4ed389c259d
irfanhusaini <irfan0102660714@gmail.com>
2025-03-07 08:20:53 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_5/CHAPTER4-TASK5-MUHAMMAD IRFAN.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tasklooping1;
+
+/**
+ *
+ * @author irfan
+ */
+public class TaskLooping5 {
+    public static void main(String[] args) {
+     
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " "); 
+        }
+    }
+}
