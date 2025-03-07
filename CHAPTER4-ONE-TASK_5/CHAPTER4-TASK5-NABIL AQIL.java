cee48739f5f748ce6757b2c7072d763faf2be040
Goat390 <nabil.azlan79@gmail.com>
2025-03-07 10:11:06 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_5/CHAPTER4-TASK5-NABIL AQIL.java
@@ -0,0 +1,14 @@
+package javaapp;
+
+/**
+ *
+ * @author Nabil
+ */
+public class Task {
+    public static void main(String[] args) {
+        //Using "for loop" to display
+        for (int i = 99; i >= 11; i -= 11) {
+            System.out.print(i + " ");
+        }
+    }
+}
