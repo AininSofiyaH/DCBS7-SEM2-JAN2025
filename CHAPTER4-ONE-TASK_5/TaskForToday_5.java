a4dab5bc68b98925e7fe8d1712b99310ad818025
Fatihah-152 <fhusna152@gmail.com>
2025-03-08 22:14:45 +0800

    Add files via upload

---
+++ CHAPTER4-ONE-TASK_5/TaskForToday_5.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chapter.pkg4;
+
+/**
+ *
+ * @author User
+ */
+public class TaskForToday_5 {
+    public static void main(String[] args) {
+        
+        //for loop
+        
+        int count, gandaan, sum;        //declare dulu
+        
+        System.out.println("for loop");
+        System.out.println("Nombor gandaan 11");{
+
+        count = 11;                      //samakan dgn 11 supaya dia start kira dkt 11
+        for(count = 11; count <= 100; count += 11){     //baca semua syntax dulu baru print pastu baru ulang 
+            
+            sum = count + 11;            //formula nk cari gandaan 11 
+            System.out.println(count);   //count sbb kite simpan nom tu dlm count yg (count += 11)
+             
+        }}                              
+     
+       System.out.println("\n");
+       
+    }
+    
+}
